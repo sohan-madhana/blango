7b6536a028adb19c1f29755c87053d74633c0d87
sohan madhana <sohanmadhana@gmail.com>
2025-03-03 15:52:42 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -39,6 +39,46 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
 
+    ADMINS = [("Ben Shaw", "ben@example.com"), ("Leo Lucio", "leo@example.com")]
+
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {
+            "require_debug_false": {
+                "()": "django.utils.log.RequireDebugFalse",
+            },
+        },
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+            "mail_admins": {
+                "level": "ERROR",
+                "class": "django.utils.log.AdminEmailHandler",
+                "filters": ["require_debug_false"],
+            },
+        },
+        "loggers": {
+            "django.request": {
+                "handlers": ["mail_admins"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    }
 
     # Application definition
 
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render, get_object_or_404
+import logging
 
-
+logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    logger.info(
+    "Created comment on Post %d for user %s", post.pk, request.user)
     return render(request, "blog/post-detail.html", {"post": post})
