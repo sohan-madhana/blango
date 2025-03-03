1a9561dbe63aafde9c0cfcb576a5c9efe045e0ec
sohan madhana <sohanmadhana@gmail.com>
2025-03-03 19:59:27 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -102,6 +102,14 @@ class Dev(Configuration):
         # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ]
 
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
+
     ROOT_URLCONF = 'blango.urls'
 
     TEMPLATES = [
